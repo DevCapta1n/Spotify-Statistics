5b105fd1fb6cab201d82943935d10ae87ad0b75f
JackWinford <winforj2@wwu.edu>
2021-07-07 02:53:57 -0700

    completed markdown file

--- app_tests.py
+++ app_tests.py
@@ -22,7 +22,11 @@ class StatifyAppTestCase(TestCase):
         self.uid = 94566
         u = User(
             display_name = "test_user",
-            profile_pic_url = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+            profile_pic_url = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png",
+            token = "fakeToken",
+            country = "US",
+            spotify_link = "google.com",
+            followers = 9
         )
         db.session.add(u)
         u.id = self.uid
@@ -43,17 +47,8 @@ class StatifyAppTestCase(TestCase):
             resp = c.get("/")
 
             self.assertIn("Welcome to Statify", str(resp.data))
-            self.assertIn("Login with Spotify", str(resp.data))
-            self.assertIn("Created By: Jack Winford", str(resp.data))
-
-    def test_stats_home(self):
-        """test that the /statistics-home page is displaying the home page correctly"""
-        with self.client as c:
-            resp = c.get("/statistics-home/94566")
-
-            self.assertIn("test_user", str(resp.data))
-            self.assertIn("Select A Time Range for Artists", str(resp.data))
-            self.assertIn("Statify", str(resp.data))
+            self.assertIn("login with Spotify", str(resp.data))
+            self.assertIn("created by: Jack Winford", str(resp.data))
 
 if __name__ == '__main__':
     unittest.main()
